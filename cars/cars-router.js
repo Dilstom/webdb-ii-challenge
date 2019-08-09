8af343b9f1fc02c2b9eb054d954c0d253993357c
Dil Sombayeva <dilyasom@gmail.com>
2019-08-09 12:10:33 -0700

    delete req

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -36,6 +36,18 @@ router.post('/', async (req, res) => {
  }
 });
 
+router.delete('/:id', async (req, res) => {
+ try {
+  const { id } = req.params;
+  const deleted = await carsDb('cars')
+   .where({ id })
+   .del();
+  res.status(200).json({ message: 'Successfully deleted the record', deleted });
+ } catch (err) {
+  res.status(500).json({ message: 'Failed to delete this car' });
+ }
+});
+
 });
 
 module.exports = router;
