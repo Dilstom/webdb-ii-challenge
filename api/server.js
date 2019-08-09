3d1b52503e1cf5a0164a0778e2f4b0495404ab0f
Dil Sombayeva <dilyasom@gmail.com>
2019-08-09 11:46:53 -0700

    add server.js

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const carsRouter = require('../cars/cars-router');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/cars', carsRouter);
+
+server.get('/', (req, res) => {
+ res.send('<h2>Home Page!</h2>');
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const express = require('express');
-
-const server = express();
+const server = require('./api/server');
+// const server = express();
 
 const port = process.env.PORT || 5001;
 server.listen(port, () => console.log(`App is running on port ${port}`));
