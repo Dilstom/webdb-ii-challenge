7a3b71e6622408de0db35bb31429b4cd790c6f00
Dil Sombayeva <dilyasom@gmail.com>
2019-08-09 12:09:49 -0700

    get by id req

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -12,6 +12,18 @@ router.get('/', async (req, res) => {
  }
 });
 
+router.get('/:id', async (req, res) => {
+ try {
+  const { id } = req.params;
+  const car = await carsDb('cars').where({ id });
+  res.status(200).json(car);
+ } catch (err) {
+  res
+   .status(500)
+   .json({ message: 'Failed to retrieve car with this specified id' });
+ }
+});
+
 router.post('/', async (req, res) => {
  try {
  } catch (err) {}
