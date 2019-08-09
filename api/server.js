efc08415a45b19645fac2f2220b2c2a7a4b84c4b
Dil Sombayeva <dilyasom@gmail.com>
2019-08-09 12:12:26 -0700

    add helmet

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const carsRouter = require('../cars/cars-router');
+const helmet = require('helmet');
 
 const server = express();
 
+server.use(helmet());
 server.use(express.json());
 server.use('/api/cars', carsRouter);
 
