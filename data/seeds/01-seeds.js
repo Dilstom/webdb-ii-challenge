d1db6fa70470fc4fb197a1a80c7d66b5960fd30a
Dil Sombayeva <dilyasom@gmail.com>
2019-08-09 17:07:48 -0700

    add more data in seeds

--- data/seeds/01-seeds.js
+++ data/seeds/01-seeds.js
@@ -10,18 +10,22 @@ exports.seed = function(knex) {
      make: 'Mercedes',
      model: 'C-class',
      mileage: '15000',
+     title: 'clean',
+     transmission: 'auto',
     },
     {
      VIN: '23DFDS789HJKl',
      make: 'BMW',
      model: 'x5',
      mileage: '49000',
+     title: 'clean',
     },
     {
      VIN: '23YHEIWOR899090',
      make: 'BMW',
      model: 'i8',
      mileage: '32000',
+     title: 'salvage',
     },
    ]);
   });
